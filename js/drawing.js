887a2d01cb39c31d8c9ca219d4660b546f03b907
Bartosz Mężyk <bartmezyk@gmail.com>
2021-05-22 18:01:34 +0200

    Create variables.js and drawing.js.

---
+++ js/drawing.js
@@ -0,0 +1,26 @@
+const drawing = () =>
+{
+	let drawedNumber;
+
+	if (sequence.length==0) drawedNumber = Math.floor(Math.random() * 4); //Gdy pierwszy raz włączamy grę następuje losowanie z wszystkich 4 postaci.
+	else //W przeciwnym wypadku poprzednio wylosowana postać ma nie być brana pod uwagę przy kolejnym losowaniu.
+	{
+		const numbersForDrawing = []; //Tymczasowa tablica numerów postaci
+
+		for(i = 0; i < 4; i++)
+		{
+			if(i != sequence[sequence.length - 1]) ///Tylko numer postaci poprzednio wylosowanej nie znajdzie sie w tymczasowej tablicy numerów postaci do kolejnego losowania.
+			{
+				numbersForDrawing.push(i);
+			}
+		}
+		const drawedArrayIndex = Math.floor(Math.random() * 3); //Losowanie tylko spośród 3 liczb tym razem.
+		
+		drawedNumber = numbersForDrawing[drawedArrayIndex];
+	}
+	sequence.push(drawedNumber); //Umieszczenie wylosowanego nr postaci w globalnej tablicy liczb będących sekwencją numerów postaci do kliknięcia.
+	
+	const drawedSound = (Math.floor(Math.random() * 5)).toString(); //Losowanie nr dźwięku spośród 5 możliwych.
+	const soundName = drawedNumber+drawedSound; //Powiązanie wylosowanego nr postaci z wylosowanym nr dźwięku.
+	sequenceSound.push(soundName); //Umieszczenie połączonego nr w globalnej tablicy dźwięków sekwencyjnie wydawanych przez postacie.
+}
---
+++ js/variables.js
@@ -0,0 +1,4 @@
+const sequenceNumber = []; //Tablica liczb będących sekwencją numerów postaci do kliknięcia.
+const sequenceSound = []; //Tablica liczb będących nazwami dźwięków wydawanych sekwencyjnie przez postacie.
+
+const gameInfo = document.querySelector('.gameInfo');
